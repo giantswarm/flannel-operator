d85d07bb734510efe79bbc37fbc6e1a80cb0a65b
Paweł Kopiczko <kopiczko@gmail.com>
2017-07-13 16:37:37 +0200

    add godoc

--- service/flannel/namespace.go
+++ service/flannel/namespace.go
@@ -6,6 +6,8 @@ import (
 	apiv1 "k8s.io/client-go/pkg/api/v1"
 )
 
+// newNamespace creates a namespace with a given name. The created namespace
+// has a commont set of labels for this operator.
 func newNamespace(spec flanneltpr.Spec, name string) *apiv1.Namespace {
 	return &apiv1.Namespace{
 		TypeMeta: apismetav1.TypeMeta{
