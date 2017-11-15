c0147b2c2d281d06f3cbe942c212bbf6ddee94fc
Tim Schindler <xh3b4sd@gmail.com>
2017-11-15 12:00:49 +0100

    use unified operatorkit framework (#71)

--- vendor/github.com/giantswarm/flanneltpr/spec.go
+++ vendor/github.com/giantswarm/flanneltpr/spec.go
@@ -1,11 +1,15 @@
 package flanneltpr
 
-import "github.com/giantswarm/flanneltpr/spec"
+import (
+	"github.com/giantswarm/versionbundle"
+
+	"github.com/giantswarm/flanneltpr/spec"
+)
 
 type Spec struct {
-	Cluster spec.Cluster `json:"cluster" yaml:"cluster"`
-
-	Bridge  spec.Bridge  `json:"bridge" yaml:"bridge"`
-	Health  spec.Health  `json:"health" yaml:"health"`
-	Flannel spec.Flannel `json:"flannel" yaml:"flannel"`
+	Bridge        spec.Bridge          `json:"bridge" yaml:"bridge"`
+	Cluster       spec.Cluster         `json:"cluster" yaml:"cluster"`
+	Health        spec.Health          `json:"health" yaml:"health"`
+	Flannel       spec.Flannel         `json:"flannel" yaml:"flannel"`
+	VersionBundle versionbundle.Bundle `json:"version_bundle" yaml:"version_bundle"`
 }
