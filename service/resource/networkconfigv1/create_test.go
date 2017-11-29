af579813e73c2a074e51b6d8e46b576e4049b33a
Tim Schindler <xh3b4sd@gmail.com>
2017-11-29 12:16:10 +0100

    versioned resources (#78)

---
+++ service/resource/networkconfigv1/create_test.go
@@ -0,0 +1,129 @@
+package networkconfigv1
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/giantswarm/flanneltpr"
+	"github.com/giantswarm/micrologger/microloggertest"
+
+	etcdv2fake "github.com/giantswarm/flannel-operator/service/etcdv2/fake"
+)
+
+func Test_Resource_NetworkConfig_newCreateChange(t *testing.T) {
+	testCases := []struct {
+		Obj                   interface{}
+		CurrentState          interface{}
+		DesiredState          interface{}
+		ExpectedNetworkConfig NetworkConfig
+	}{
+		// Test 0 ensures that zero value inputs result in zero value outputs.
+		{
+			Obj:                   &flanneltpr.CustomObject{},
+			CurrentState:          NetworkConfig{},
+			DesiredState:          NetworkConfig{},
+			ExpectedNetworkConfig: NetworkConfig{},
+		},
+
+		// Test 1 ensures that the create state matches the desired state in case
+		// the current state is empty.
+		{
+			Obj:          &flanneltpr.CustomObject{},
+			CurrentState: NetworkConfig{},
+			DesiredState: NetworkConfig{
+				Network:   "172.26.0.0/16",
+				SubnetLen: 30,
+				Backend: Backend{
+					Type: "vxlan",
+					VNI:  26,
+				},
+			},
+			ExpectedNetworkConfig: NetworkConfig{
+				Network:   "172.26.0.0/16",
+				SubnetLen: 30,
+				Backend: Backend{
+					Type: "vxlan",
+					VNI:  26,
+				},
+			},
+		},
+
+		// Test 2 ensures that the create state is empty in case the current state
+		// is not empty.
+		{
+			Obj: &flanneltpr.CustomObject{},
+			CurrentState: NetworkConfig{
+				Network:   "172.26.0.0/16",
+				SubnetLen: 30,
+				Backend: Backend{
+					Type: "vxlan",
+					VNI:  26,
+				},
+			},
+			DesiredState: NetworkConfig{
+				Network:   "172.26.0.0/16",
+				SubnetLen: 30,
+				Backend: Backend{
+					Type: "vxlan",
+					VNI:  26,
+				},
+			},
+			ExpectedNetworkConfig: NetworkConfig{},
+		},
+
+		// Test 3 ensures that the create state is empty in case the current state
+		// is not empty. NOTE that this is like test 2 but with incomplete or wrong
+		// configuration. The creation process only takes care about configuration
+		// existance. The update process takes care about keeping the existing
+		// configuration in the right shape.
+		{
+			Obj: &flanneltpr.CustomObject{},
+			CurrentState: NetworkConfig{
+				Backend: Backend{
+					Type: "vxlan",
+				},
+			},
+			DesiredState: NetworkConfig{
+				Network:   "172.26.0.0/16",
+				SubnetLen: 30,
+				Backend: Backend{
+					Type: "vxlan",
+					VNI:  26,
+				},
+			},
+			ExpectedNetworkConfig: NetworkConfig{},
+		},
+	}
+
+	var err error
+	var newResource *Resource
+	{
+		resourceConfig := DefaultConfig()
+
+		resourceConfig.Logger = microloggertest.New()
+		resourceConfig.Store = etcdv2fake.New()
+
+		newResource, err = New(resourceConfig)
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+	}
+
+	for _, tc := range testCases {
+		createState, err := newResource.newCreateChange(context.TODO(), tc.Obj, tc.CurrentState, tc.DesiredState)
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		t.Run("ValidateCreateState", func(t *testing.T) {
+			networkConfigToCreate, ok := createState.(NetworkConfig)
+			if !ok {
+				t.Fatalf("expected %T got %T", NetworkConfig{}, createState)
+			}
+			if !reflect.DeepEqual(networkConfigToCreate, tc.ExpectedNetworkConfig) {
+				t.Fatalf("expected %#v got %#v", tc.ExpectedNetworkConfig, networkConfigToCreate)
+			}
+		})
+	}
+}
---
+++ service/resource/networkconfigv1/resource.go
@@ -0,0 +1,74 @@
+package networkconfigv1
+
+import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/framework"
+
+	"github.com/giantswarm/flannel-operator/service/etcdv2"
+)
+
+const (
+	// BackendType is the backend type the flannel network is configured with.
+	BackendType = "vxlan"
+	// Name is the identifier of the resource.
+	Name = "networkconfig"
+)
+
+// Config represents the configuration used to create a new network config
+// resource.
+type Config struct {
+	Logger micrologger.Logger
+	Store  etcdv2.Store
+}
+
+// DefaultConfig provides a default configuration to create a new network config
+// resource by best effort.
+func DefaultConfig() Config {
+	return Config{
+		Logger: nil,
+		Store:  nil,
+	}
+}
+
+// Resource implements the network config resource.
+type Resource struct {
+	logger micrologger.Logger
+	store  etcdv2.Store
+}
+
+// New creates a new configured network config resource.
+func New(config Config) (*Resource, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
+	if config.Store == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Store must not be empty")
+	}
+
+	newResource := &Resource{
+		logger: config.Logger.With(
+			"resource", Name,
+		),
+		store: config.Store,
+	}
+
+	return newResource, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
+
+func (r *Resource) Underlying() framework.Resource {
+	return r
+}
+
+func toNetworkConfig(v interface{}) (NetworkConfig, error) {
+	networkConfig, ok := v.(NetworkConfig)
+	if !ok {
+		return NetworkConfig{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &NetworkConfig{}, v)
+	}
+
+	return networkConfig, nil
+}
