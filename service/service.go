f5297f35b19fe9e2685cbe0b9b1ecb2fc3575e59
Tim Schindler <xh3b4sd@gmail.com>
2017-12-11 10:07:07 +0100

    disabled TPR reconciliation

--- service/service.go
+++ service/service.go
@@ -152,6 +152,6 @@ func New(config Config) (*Service, error) {
 func (s *Service) Boot() {
 	s.bootOnce.Do(func() {
 		go s.CRDFramework.Boot()
-		go s.customObjectFramework.Boot()
+		//go s.customObjectFramework.Boot()
 	})
 }
