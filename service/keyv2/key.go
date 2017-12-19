6ae611838019ca1b8132bf1a6cb5e70cf5af4c61
Tim Schindler <xh3b4sd@gmail.com>
2017-12-19 14:47:32 +0100

    compute desired state for flanneldv1 resource

--- service/keyv2/key.go
+++ service/keyv2/key.go
@@ -1,6 +1,9 @@
 package keyv2
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
 	"github.com/giantswarm/microerror"
 )
@@ -31,12 +34,72 @@ func EtcdNetworkPath(customObject v1alpha1.FlannelConfig) string {
 	return "coreos.com/network/" + NetworkBridgeName(customObject)
 }
 
+func EtcdPrefix(customObject v1alpha1.FlannelConfig) string {
+	return "/" + EtcdNetworkPath(customObject)
+}
+
+func FlannelRunDir(customObject v1alpha1.FlannelConfig) string {
+	return customObject.Spec.Flannel.Spec.RunDir
+}
+
+func FlannelVNI(customObject v1alpha1.FlannelConfig) int {
+	return customObject.Spec.Flannel.Spec.VNI
+}
+
+func HostPrivateNetwork(customObject v1alpha1.FlannelConfig) string {
+	return customObject.Spec.Bridge.Spec.PrivateNetwork
+}
+
+func NetworkBridgeDockerImage(customObject v1alpha1.FlannelConfig) string {
+	return customObject.Spec.Bridge.Docker.Image
+}
+
 func NetworkBridgeName(customObject v1alpha1.FlannelConfig) string {
 	return "br-" + ClusterID(customObject)
 }
 
+func NetworkDNSBlock(customObject v1alpha1.FlannelConfig) string {
+	var parts []string
+
+	for _, s := range customObject.Spec.Bridge.Spec.DNS.Servers {
+		parts = append(parts, fmt.Sprintf("DNS=%s", s))
+	}
+
+	return strings.Join(parts, "\n")
+}
+
+func NetworkEnvFilePath(customObject v1alpha1.FlannelConfig) string {
+	return fmt.Sprintf("%s/networks/%s.env", FlannelRunDir(customObject), NetworkBridgeName(customObject))
+}
+
+func NetworkFlannelDevice(customObject v1alpha1.FlannelConfig) string {
+	return fmt.Sprintf("flannel.%d", FlannelVNI(customObject))
+}
+
+func NetworkHealthDockerImage(customObject v1alpha1.FlannelConfig) string {
+	return customObject.Spec.Health.Docker.Image
+}
+
+func NetworkInterfaceName(customObject v1alpha1.FlannelConfig) string {
+	return customObject.Spec.Bridge.Spec.Interface
+}
+
 func NetworkNamespace(customObject v1alpha1.FlannelConfig) string {
 	return NetworkID + "-" + ClusterID(customObject)
+}
+
+func NetworkNTPBlock(customObject v1alpha1.FlannelConfig) string {
+	var parts []string
+
+	for _, s := range customObject.Spec.Bridge.Spec.NTP.Servers {
+		parts = append(parts, fmt.Sprintf("NTP=%s", s))
+	}
+
+	return strings.Join(parts, "\n")
+}
+
+func NetworkTapName(customObject v1alpha1.FlannelConfig) string {
+	return "tap-" + ClusterID(customObject)
 }
 
 func ToCustomObject(v interface{}) (v1alpha1.FlannelConfig, error) {
--- service/resource/flanneldv1/resource.go
+++ service/resource/flanneldv1/resource.go
@@ -15,31 +15,39 @@ const (
 // Config represents the configuration used to create a new cloud config
 // resource.
 type Config struct {
-	// Dependencies.
 	K8sClient kubernetes.Interface
 	Logger    micrologger.Logger
+
+	EtcdCAFile  string
+	EtcdCrtFile string
+	EtcdKeyFile string
 }
 
 // DefaultConfig provides a default configuration to create a new cloud config
 // resource by best effort.
 func DefaultConfig() Config {
 	return Config{
-		// Dependencies.
 		K8sClient: nil,
 		Logger:    nil,
+
+		EtcdCAFile:  "",
+		EtcdCrtFile: "",
+		EtcdKeyFile: "",
 	}
 }
 
 // Resource implements the cloud config resource.
 type Resource struct {
-	// Dependencies.
 	k8sClient kubernetes.Interface
 	logger    micrologger.Logger
+
+	etcdCAFile  string
+	etcdCrtFile string
+	etcdKeyFile string
 }
 
 // New creates a new configured cloud config resource.
 func New(config Config) (*Resource, error) {
-	// Dependencies.
 	if config.K8sClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "config.K8sClient must not be empty")
 	}
@@ -47,15 +55,28 @@ func New(config Config) (*Resource, error) {
 		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
 	}
 
-	newService := &Resource{
-		// Dependencies.
+	if config.EtcdCAFile == "" {
+		return nil, microerror.Maskf(invalidConfigError, "config.EtcdCAFile must not be empty")
+	}
+	if config.EtcdCrtFile == "" {
+		return nil, microerror.Maskf(invalidConfigError, "config.EtcdCrtFile must not be empty")
+	}
+	if config.EtcdKeyFile == "" {
+		return nil, microerror.Maskf(invalidConfigError, "config.EtcdKeyFile must not be empty")
+	}
+
+	r := &Resource{
 		k8sClient: config.K8sClient,
 		logger: config.Logger.With(
 			"resource", Name,
 		),
+
+		etcdCAFile:  config.EtcdCAFile,
+		etcdCrtFile: config.EtcdCrtFile,
+		etcdKeyFile: config.EtcdKeyFile,
 	}
 
-	return newService, nil
+	return r, nil
 }
 
 func (r *Resource) Name() string {
