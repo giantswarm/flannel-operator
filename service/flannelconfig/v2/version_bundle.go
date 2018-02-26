efc10263ee781ec4101148498edab2cecfcc864e
Tim Schindler <xh3b4sd@gmail.com>
2018-02-26 10:19:18 +0100

    moved version bundle to versioned package

---
+++ service/flannelconfig/v2/version_bundle.go
@@ -0,0 +1,36 @@
+package v2
+
+import (
+	"time"
+
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "flannel",
+				Description: "Flannel version updated.",
+				Kind:        "changed",
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "flannel",
+				Version: "0.9.0",
+			},
+		},
+		Dependencies: []versionbundle.Dependency{
+			{
+				Name:    "etcd",
+				Version: "<= 3.2.x",
+			},
+		},
+		Deprecated: false,
+		Name:       "flannel-operator",
+		Time:       time.Date(2017, time.October, 27, 16, 21, 0, 0, time.UTC),
+		Version:    "0.1.0",
+		WIP:        false,
+	}
+}
--- service/version_bundles.go
+++ service/version_bundles.go
@@ -1,38 +1,15 @@
 package service
 
 import (
-	"time"
-
 	"github.com/giantswarm/versionbundle"
+
+	"github.com/giantswarm/flannel-operator/service/flannelconfig/v2"
 )
 
 func NewVersionBundles() []versionbundle.Bundle {
-	return []versionbundle.Bundle{
-		{
-			Changelogs: []versionbundle.Changelog{
-				{
-					Component:   "flannel",
-					Description: "Flannel version updated.",
-					Kind:        "changed",
-				},
-			},
-			Components: []versionbundle.Component{
-				{
-					Name:    "flannel",
-					Version: "0.9.0",
-				},
-			},
-			Dependencies: []versionbundle.Dependency{
-				{
-					Name:    "etcd",
-					Version: "<= 3.2.x",
-				},
-			},
-			Deprecated: false,
-			Name:       "flannel-operator",
-			Time:       time.Date(2017, time.October, 27, 16, 21, 0, 0, time.UTC),
-			Version:    "0.1.0",
-			WIP:        false,
-		},
-	}
+	var versionBundles []versionbundle.Bundle
+
+	versionBundles = append(versionBundles, v2.VersionBundle())
+
+	return versionBundles
 }
