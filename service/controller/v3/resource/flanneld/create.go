134ea57f4180e42af609ba23d7c68243aba56af3
Tim Schindler <xh3b4sd@gmail.com>
2018-04-12 13:39:47 +0200

    renames flannelconfig to controller (#131)

---
+++ service/controller/v3/resource/flanneld/create.go
@@ -0,0 +1,62 @@
+package flanneld
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+	"k8s.io/api/extensions/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/giantswarm/flannel-operator/service/controller/v3/key"
+)
+
+func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange interface{}) error {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	daemonSetToCreate, err := toDaemonSet(createChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if daemonSetToCreate != nil {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "creating the daemon set in the Kubernetes API")
+
+		namespace := key.NetworkNamespace(customObject)
+		_, err = r.k8sClient.Extensions().DaemonSets(namespace).Create(daemonSetToCreate)
+		if apierrors.IsAlreadyExists(err) {
+			// fall through
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "created the daemon set in the Kubernetes API")
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "the daemon set does not need to be created in the Kubernetes API")
+	}
+
+	return nil
+}
+
+func (r *Resource) newCreateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentDaemonSet, err := toDaemonSet(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredDaemonSet, err := toDaemonSet(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "finding out if the daemon set has to be created")
+
+	var daemonSetToCreate *v1beta1.DaemonSet
+	if currentDaemonSet == nil {
+		daemonSetToCreate = desiredDaemonSet
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "found out if the daemon set has to be created")
+
+	return daemonSetToCreate, nil
+}
---
+++ service/controller/v3/resource/networkconfigv2/resource.go
@@ -0,0 +1,69 @@
+package networkconfigv2
+
+import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+
+	"github.com/giantswarm/flannel-operator/service/controller/v3/etcd"
+)
+
+const (
+	// BackendType is the backend type the flannel network is configured with.
+	BackendType = "vxlan"
+	// Name is the identifier of the resource.
+	Name = "networkconfigv2"
+)
+
+// Config represents the configuration used to create a new network config
+// resource.
+type Config struct {
+	Logger micrologger.Logger
+	Store  etcd.Store
+}
+
+// DefaultConfig provides a default configuration to create a new network config
+// resource by best effort.
+func DefaultConfig() Config {
+	return Config{
+		Logger: nil,
+		Store:  nil,
+	}
+}
+
+// Resource implements the network config resource.
+type Resource struct {
+	logger micrologger.Logger
+	store  etcd.Store
+}
+
+// New creates a new configured network config resource.
+func New(config Config) (*Resource, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
+	}
+	if config.Store == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Store must not be empty")
+	}
+
+	newResource := &Resource{
+		logger: config.Logger.With(
+			"resource", Name,
+		),
+		store: config.Store,
+	}
+
+	return newResource, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
+
+func toNetworkConfig(v interface{}) (NetworkConfig, error) {
+	networkConfig, ok := v.(NetworkConfig)
+	if !ok {
+		return NetworkConfig{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &NetworkConfig{}, v)
+	}
+
+	return networkConfig, nil
+}
