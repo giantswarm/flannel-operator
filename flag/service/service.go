92d346008f5720aec6b33cbd05bdf7e6b4e43c8b
Tuomas Makinen <tuommaki@gmail.com>
2018-10-02 13:26:26 +0300

    Add CRD label selector for e2e tests
    On KVM e2e tests there are multiple instances of operators running at
    the same time. In order to prevent race conditions, add label selector
    to informer to only process CRs meant for current instance of operator.

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,11 +1,13 @@
 package service
 
 import (
+	"github.com/giantswarm/flannel-operator/flag/service/crd"
 	"github.com/giantswarm/flannel-operator/flag/service/etcd"
 	"github.com/giantswarm/flannel-operator/flag/service/kubernetes"
 )
 
 type Service struct {
+	CRD        crd.CRD
 	Etcd       etcd.Etcd
 	Kubernetes kubernetes.Kubernetes
 }
