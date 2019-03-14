64468305d124584bcb2960543a35280f4866b4b0
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-03-14 12:37:26 +0100

    Add kubeconfig support (#177)
    * vendor: updated operatorkit
    
    * vendor: updated operatorkit
    
    * Flags changes
    
    * Add missing kubeconfig setting
    
    * dep: remove pin

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,9 +1,10 @@
 package service
 
 import (
+	"github.com/giantswarm/operatorkit/flag/service/kubernetes"
+
 	"github.com/giantswarm/flannel-operator/flag/service/crd"
 	"github.com/giantswarm/flannel-operator/flag/service/etcd"
-	"github.com/giantswarm/flannel-operator/flag/service/kubernetes"
 )
 
 type Service struct {
